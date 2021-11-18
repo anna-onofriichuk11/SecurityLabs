47b1262546922dc2b506d2f763c09575d7ffddd2
Anna Onofriichuk <Anna_Onofriichuk@epam.com>
2021-11-18 17:23:53 +0200

    Task2 updated

--- lab1/lab1/lab1/Vigenere.cs
+++ lab1/lab1/lab1/Vigenere.cs
@@ -1,10 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Text;
+using static lab1.HelpCLass;
+using ConsoleTables;
 
 namespace lab1
 {
-    class Vigenere
+  public static class Vigenere
     {
+      
+        public static Dictionary<int, Tuple<double, double>> HammingDistance(byte[] bytearr)
+        {
+            int maxKeySize = 20;
+            int currKeySize = 2;
+            var hammingDistance = new Dictionary<int, Tuple<double, double>>();
+            while (currKeySize < maxKeySize)
+            {
+                int bytesCount = 0;
+                var distancesList = new List<double>();
+                for (int i = 0; i < bytearr.Length; i += currKeySize)
+                {
+                    if (i + (2 * currKeySize) > bytearr.Length)
+                        continue;
+                    var bytes1 = new List<byte>();
+                    var bytes2 = new List<byte>();
+
+                    for (int j = i; j < i + currKeySize; j++)
+                    {
+                        bytes1.Add(bytearr[j]);
+                    }
+                    for (int n = i + currKeySize; n < i + currKeySize + currKeySize; n++)
+                    {
+                        bytes2.Add(bytearr[n]);
+                    }
+                    distancesList.Add(VigenereHelpClass.BitwiseHamming(bytes1, bytes2));
+                    bytesCount++;
+                }
+                hammingDistance.Add(currKeySize, new Tuple<double, double>(distancesList.Sum() / bytesCount, (distancesList.Sum() / bytesCount) / currKeySize));
+                currKeySize++;
+            }
+
+            return hammingDistance;
+        }
+
+        public static string Decrypt(string keyLength, string input)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                sb.Append((char)(input[i] ^ keyLength[i % keyLength.Length]));
+            }
+            return sb.ToString();
+        }
+
+        public static byte DecodeRepeatingXOR(List<byte> bytearr)
+        {
+            var topSymbols = bytearr.Take(6).ToList();
+            var letters = new char[] { 'a', 'e', 'i', 'n', 'o', 't' };
+            var letterBytes = letters.Select(c => (byte)c).ToList();
+
+            var listCounts = new List<byte>();
+            for (int i = 0; i < letterBytes.Count; i++)
+            {
+               // Console.WriteLine($"letter: {(char)letterBytes[i]}");
+                for (int j = 0; j < topSymbols.Count; j++)
+                {
+                    var xor = topSymbols[j] ^ letterBytes[i];
+                    listCounts.Add(Convert.ToByte(xor));
+             //       Console.Write($"\nSymbol: {topSymbols[j]}\t\tXOR: {xor}");
+                }
+              //  Console.WriteLine();
+            }
+            var probableKeyLetters = GetByteFrequency(listCounts);
+            var table = new ConsoleTable("Symbol", "Count");
+            foreach (var dict in probableKeyLetters)
+                table.AddRow(Convert.ToChar(dict.Key), dict.Value);
+            table.Write();
+            return probableKeyLetters.First().Key;
+        }
+
+        public static Dictionary<byte, int> GetByteFrequency(List<byte> bytearr)
+        {
+            var fr = bytearr.GroupBy(c => c)
+                        .Select(g => new { Symbol = g.Key, Count = g.Count() })
+                        .OrderByDescending(b => b.Count)
+                        .ToDictionary(c => c.Symbol, c => c.Count);
+            //foreach (var item in freqs)
+            //{
+            //    Console.WriteLine($"Symbol: {item.Key}, Count: {item.Value}");
+            //}
+            return fr;
+        }
+
+        public static string GetBestKey(byte[] arr, int keyLength)
+        {
+            var firstPart = VigenereHelpClass.BytePartition(arr.ToList(), 0, keyLength);
+            var secondPart = VigenereHelpClass.BytePartition(arr.ToList(), 1, keyLength);
+            var thirdPart = VigenereHelpClass.BytePartition(arr.ToList(), 2, keyLength);
+
+            var firstSymbols = Vigenere.GetByteFrequency(firstPart).Keys.ToList();
+            var secondSymbols = Vigenere.GetByteFrequency(secondPart).Keys.ToList();
+            var thirdSymbols = Vigenere.GetByteFrequency(thirdPart).Keys.ToList();
+
+           // Console.WriteLine("---------------------------------------------first:---------------------------------------------");
+            var firstLetter = Vigenere.DecodeRepeatingXOR(firstSymbols);
+          //  Console.WriteLine("---------------------------------------------second:---------------------------------------------");
+            var secondLetter = Vigenere.DecodeRepeatingXOR(secondSymbols);
+          //  Console.WriteLine("---------------------------------------------third:---------------------------------------------");
+            var thirdLetter = Vigenere.DecodeRepeatingXOR(thirdSymbols);
+
+            var keyWord = string.Format($"{(char)firstLetter}{(char)secondLetter}{(char)thirdLetter}");
+            Console.WriteLine( "KEY WORD " + keyWord);
+
+            return keyWord;
+        }
+       
     }
 }
---
+++ lab1/lab1/lab1/VigenereHelpClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    public class VigenereHelpClass
+    {
+        public static double BitwiseHamming(List<byte> x, List<byte> y)
+        {
+            byte[] nbits =
+           {
+            0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+            1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+            1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+            1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+            2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+            3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+            4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8,
+            };
+
+
+            int d = 0;
+
+            for (int i = 0; i < x.Count; i++)
+            {
+                byte xorByte = (byte)(x[i] ^ y[i]);
+                d += nbits[xorByte];
+            }
+            return d;
+        }
+
+        public static List<byte> BytePartition(List<byte> bytearr, int index, int keyLength)
+        {
+            var resArr = new List<byte>();
+            for (int i = index; i < bytearr.Count; i += keyLength)
+            {
+                resArr.Add(bytearr[i]);
+            }
+            return resArr;
+        }
+
+    }
+
+}
